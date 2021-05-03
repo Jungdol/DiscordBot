4a619929053d12389e0ca7a63999934fa111691a
Jungyeop Kim <61561973+Jungdol@users.noreply.github.com>
2021-05-03 23:53:08 +0900

    DiscordBot v1.5
    jsonSetting\subject.py 부분 중 쓸데없이 반복되는 부분 최적화

--- jsonSetting/subject.py
+++ jsonSetting/subject.py
@@ -1,58 +1,66 @@
+def subjects(name, teacher, how, url):  # json 과목 입력
+    return {"name": name, "teacher": teacher, "how": how, "url": url}
+
+
+def test():  # 과목 입력 예시
+    return subjects("과학", "김과학", "Zoom", "https://asdf.asdf.com")
+
+
 def ApplicationProgramming():  # 응용 프로그래밍
-    return {"name": "과목 이름", "teacher": "담당 선생님", "how": "진행 방식 (Zoom, Google Meet)", "url": "Zoom, Google Meet 링크"}
+    return subjects("과목 이름", "담당 선생님", "진행 방식 (Zoom, Google Meet)", "Zoom, Google Meet 링크")
 
 
 def Autonomy():  # 자율
-    return {"name": "과목 이름", "teacher": "담당 선생님", "how": "진행 방식 (Zoom, Google Meet)", "url": "Zoom, Google Meet 링크"}
+    return subjects("과목 이름", "담당 선생님", "진행 방식 (Zoom, Google Meet)", "Zoom, Google Meet 링크")
 
 
 def CareerAndJob():  # 진로
-    return {"name": "과목 이름", "teacher": "담당 선생님", "how": "진행 방식 (Zoom, Google Meet)", "url": "Zoom, Google Meet 링크"}
+    return subjects("과목 이름", "담당 선생님", "진행 방식 (Zoom, Google Meet)", "Zoom, Google Meet 링크")
 
 
 def CreativeActivities():  # 창체
-    return {"name": "과목 이름", "teacher": "담당 선생님", "how": "진행 방식 (Zoom, Google Meet)", "url": "Zoom, Google Meet 링크"}
+    return subjects("과목 이름", "담당 선생님", "진행 방식 (Zoom, Google Meet)", "Zoom, Google Meet 링크")
 
 
 def English():  # 영어
-    return {"name": "과목 이름", "teacher": "담당 선생님", "how": "진행 방식 (Zoom, Google Meet)", "url": "Zoom, Google Meet 링크"}
+    return subjects("과목 이름", "담당 선생님", "진행 방식 (Zoom, Google Meet)", "Zoom, Google Meet 링크")
 
 
 def Exercise():  # 운동
-    return {"name": "과목 이름", "teacher": "담당 선생님", "how": "진행 방식 (Zoom, Google Meet)", "url": "Zoom, Google Meet 링크"}
+    return subjects("과목 이름", "담당 선생님", "진행 방식 (Zoom, Google Meet)", "Zoom, Google Meet 링크")
 
 
 def GameProgramming():  # 게임 프로그래밍
-    return {"name": "과목 이름", "teacher": "담당 선생님", "how": "진행 방식 (Zoom, Google Meet)", "url": "Zoom, Google Meet 링크"}
+    return subjects("과목 이름", "담당 선생님", "진행 방식 (Zoom, Google Meet)", "Zoom, Google Meet 링크")
 
 
 def Japanese():  # 일본어
-    return {"name": "과목 이름", "teacher": "담당 선생님", "how": "진행 방식 (Zoom, Google Meet)", "url": "Zoom, Google Meet 링크"}
+    return subjects("과목 이름", "담당 선생님", "진행 방식 (Zoom, Google Meet)", "Zoom, Google Meet 링크")
 
 
 def KoreanHistory():  # 한국사
-    return {"name": "과목 이름", "teacher": "담당 선생님", "how": "진행 방식 (Zoom, Google Meet)", "url": "Zoom, Google Meet 링크"}
+    return subjects("과목 이름", "담당 선생님", "진행 방식 (Zoom, Google Meet)", "Zoom, Google Meet 링크")
 
 
 def Literature():  # 문학
-    return {"name": "과목 이름", "teacher": "담당 선생님", "how": "진행 방식 (Zoom, Google Meet)", "url": "Zoom, Google Meet 링크"}
+    return subjects("과목 이름", "담당 선생님", "진행 방식 (Zoom, Google Meet)", "Zoom, Google Meet 링크")
 
 
 def Math():  # 수학I
-    return {"name": "과목 이름", "teacher": "담당 선생님", "how": "진행 방식 (Zoom, Google Meet)", "url": "Zoom, Google Meet 링크"}
+    return subjects("과목 이름", "담당 선생님", "진행 방식 (Zoom, Google Meet)", "Zoom, Google Meet 링크")
 
 
 def Science():  # 과학
-    return {"name": "과목 이름", "teacher": "담당 선생님", "how": "진행 방식 (Zoom, Google Meet)", "url": "Zoom, Google Meet 링크"}
+    return subjects("과목 이름", "담당 선생님", "진행 방식 (Zoom, Google Meet)", "Zoom, Google Meet 링크")
 
 
 def SmartCultureAppContentsProduction():  # 스마트문화엡콘텐츠 제작 (유서영 선생님)
-    return {"name": "과목 이름", "teacher": "담당 선생님", "how": "진행 방식 (Zoom, Google Meet)", "url": "Zoom, Google Meet 링크"}
+    return subjects("과목 이름", "담당 선생님", "진행 방식 (Zoom, Google Meet)", "Zoom, Google Meet 링크")
 
 
 def SmartCultureAppContentsProduction2():  # 스마트문화앱콘텐츠 제작 (오현지 선생님)
-    return {"name": "과목 이름", "teacher": "담당 선생님", "how": "진행 방식 (Zoom, Google Meet)", "url": "Zoom, Google Meet 링크"}
+    return subjects("과목 이름", "담당 선생님", "진행 방식 (Zoom, Google Meet)", "Zoom, Google Meet 링크")
 
 
 def Social():  # 사회
-    return {"name": "과목 이름", "teacher": "담당 선생님", "how": "진행 방식 (Zoom, Google Meet)", "url": "Zoom, Google Meet 링크"}
+    return subjects("과목 이름", "담당 선생님", "진행 방식 (Zoom, Google Meet)", "Zoom, Google Meet 링크")
