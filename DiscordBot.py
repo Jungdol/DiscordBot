be4fa3d8ca1b11c318577d0b34ff0048145074ec
Jungyeop Kim <61561973+Jungdol@users.noreply.github.com>
2021-12-20 23:31:14 +0900

    DiscordBot v2.4.0
    MyClass 함수를 MainTime 으로 변경
    조례, 종례 단어를 수정하기 쉽게 변수화
    subject.py 에서 how 를 사용하여 회의 ID와 비밀번호를 추가 가능 (당연히 json 파일로 출력 필요)
    명령어 수정
    TimeStart -> start
    TimeStop -> stop
    TimeNow -> now

--- DiscordBot.py
+++ DiscordBot.py
@@ -10,7 +10,7 @@ prefix = "!"  # 명령어 맨 앞에 붙여야 실행됨
 bot = commands.Bot(command_prefix=prefix)
 
 
-class MyClass:  # 모든 곳에 쓰여야 하기에 class 화
+class MainTime:  # 모든 곳에 쓰여야 하기에 class 화
     isTimetableStop = False
     isReturn = False
 
@@ -19,6 +19,10 @@ class MyClass:  # 모든 곳에 쓰여야 하기에 class 화
     dd = tn.strftime('%Y-%m-%d')
     ss = tn.strftime("%S")
     pass
+
+
+morning_sends = "조례 줌 들어오세요\n링크 : 줌 링크"  # 조례할 때 출력할 문장
+afternoon_sends = "종례 시간입니다. 밴드 종례 출석체크 해주세요."  # 종례할 때 출력할 문장
 
 
 def men(a):  # 멘션 기능 (저희 학과 채널은 2-8, 2-9로 운영)
@@ -35,13 +39,13 @@ def days(isReturn):  # days()는 실행 시의 요일
 
 
 def timeSet():
-    MyClass.tn = datetime.datetime.now()
-    MyClass.st = MyClass.tn.strftime("%H:%M:%S")  # "시간:분:초"로 출력 예 : 22:00:01
-    MyClass.ss = MyClass.tn.strftime("%S")  # 무분별한 업데이트를 막기 위한 초만 반환 -> 00 01 02 ...
+    MainTime.tn = datetime.datetime.now()
+    MainTime.st = MainTime.tn.strftime("%H:%M:%S")  # "시간:분:초"로 출력 예 : 22:00:01
+    MainTime.ss = MainTime.tn.strftime("%S")  # 무분별한 업데이트를 막기 위한 초만 반환 -> 00 01 02 ...
 
-    print(MyClass.st)  # 실행하면 시간:분:초 출력
-    print(MyClass.ss)  # 초단위로 출력
-    return MyClass.st  # 함수를 실행하면 시간:분:초 리턴
+    print(MainTime.st)  # 실행하면 시간:분:초 출력
+    print(MainTime.ss)  # 초단위로 출력
+    return MainTime.st  # 함수를 실행하면 시간:분:초 리턴
 
 
 async def embedSends(a, day, period):
@@ -49,10 +53,10 @@ async def embedSends(a, day, period):
 
     subject = str(variable.json_data[day][period]["name"])  # Timetable.json 안의 요일, 교시 오브젝트 안 과목 이름
     teacher = str(variable.json_data[day][period]["teacher"])  # Timetable.json 안의 요일, 교시 오브젝트 안 선생님 이름
-    # how = str(variable.json_data[day][period]["how"])  # Timetable.json 안의 요일, 교시 오브젝트 안 온라인 진행방식 (Zoom, Google Meet)
+    how = str(variable.json_data[day][period]["how"])  # Timetable.json 안의 요일, 교시 오브젝트 안 온라인 진행방식 (Zoom, Google Meet)
     url = str(variable.json_data[day][period]["url"])  # Timetable.json 안의 요일, 교시 오브젝트 안 링크
 
-    await sends(a, "{} ".format(men(a).mention) + str(subject) + " 5분뒤 " + str(teacher) + " 선생님 수업입니다.\n링크 : " + url)
+    await sends(a, "{} ".format(men(a).mention) + str(subject) + " 5분뒤 " + str(teacher) + " 선생님 수업입니다.\n" + how + "\n\n링크 : " + url)
     return None
 
 
@@ -62,7 +66,7 @@ async def sends(a, b):
 
 async def timeCheck(a, time):
     if time == str(ttb.AM_TIME):  # 조례 시간일 시 공지
-        await sends(a, str("{} 조례 줌 들어오세요\n링크 : 줌 링크".format(men(a).mention)))
+        await sends(a, str("{} " + morning_sends.format(men(a).mention)))
 
     for i in range(0, 6):  # 0~5까지 반복 총 6번 반복 -> 1~6교시
         if time == ttb.All_TIME(i):  # 만약 현재 시간이 모든 교시 시간과 같으면
@@ -78,39 +82,41 @@ async def timeCheck(a, time):
             await embedSends(a, days(True), str(period))
 
         if (time == ttb.PM_TIME("default")) and (days(True) != "Fri"):  # 위와 같이 금요일은 6교시라서 금요일은 종례 알람 꺼놓음
-            await sends(a, str("{} 종례 시간입니다. 밴드 종례 출석체크 또는 줌 들어오세요.".format(men(a).mention)))  # 종례 시간 일 시 공지
+            await sends(a, str("{} " + afternoon_sends.format(men(a).mention)))  # 종례 시간 일 시 공지
         if (time == ttb.PM_TIME("friday")) and (days(True) == "Fri"):  # 금요일 종례 알람
-            await sends(a, str("{} 종례 시간입니다. 밴드 종례 출석체크 또는 줌 들어오세요.".format(men(a).mention)))  # 종례 시간 일 시 공지
+            await sends(a, str("{} " + afternoon_sends.format(men(a).mention)))  # 종례 시간 일 시 공지
 
 
-@bot.command(name="TimeStart")
+@bot.command(name="start")
 async def Timetable(ctx):
     isOneMin = False
     await ctx.channel.purge(limit=1)  # 명령어 실행한 메세지 삭제
     # 해당 명령어 시작 시 나오는 문구
     await sends(ctx, "시간표 공지 시작합니다.")
-    MyClass.isTimetableStop = True
-    MyClass.isTimetableStop = True
+    MainTime.isTimetableStop = True
+    MainTime.isTimetableStop = True
 
     while True:
         if isOneMin:  # is OneMin == True
             await asyncio.sleep(60)  # 60초 딜레이 (1분)
             timeSet()  # 60초마다 시간 업데이트 (1분)
-            if MyClass.ss != "00":
+            if MainTime.ss != "00":
                 isOneMin = False
         elif not isOneMin:
             await asyncio.sleep(1)  # 1초 딜레이
             timeSet()  # 1초마다 시간 업데이트
-            if MyClass.ss == "00":  # 초가 00이면 (60초면)
+            if MainTime.ss == "00":  # 초가 00이면 (60초면)
                 isOneMin = True
 
-        await timeCheck(ctx, MyClass.st)
-        if (MyClass.st == ttb.All_TIME(6)) and (str(days(True)) == "Fri"):  # 금요일 6교시 끝나고 5분 뒤 알람 종료
+        await timeCheck(ctx, MainTime.st)
+
+        if (MainTime.st == ttb.All_TIME(6)) and (str(days(True)) == "Fri"):  # 금요일 6교시 끝나고 5분 뒤 알람 종료
             await sends(ctx, "온라인 수업 공지 종료합니다.")
             break
-        elif not MyClass.isTimetableStop:  # stop 명령어를 사용 시 (isTimeTableStop == False) 반복문 빠져나감.
+
+        if not MainTime.isTimetableStop:  # stop 명령어를 사용 시 (isTimeTableStop == False) 반복문 빠져나감.
             break
-        if str(MyClass.st) == "00:00:00":  # 자정일 시 days 실행 (요일 업데이트)
+        if str(MainTime.st) == "00:00:00":  # 자정일 시 days 실행 (요일 업데이트)
             days(False)
 
 
@@ -123,11 +129,11 @@ async def on_ready():  # 봇이 처음 시작 시, 재로딩 시 시작
     await bot.change_presence(status=discord.Status.online, activity=game)
     print("READY")
 
-    
-@bot.command(name="TimeStop")  # stop 명령어 실행 시 isTimetableStop = False 로 하여 시간 공지 멈춤 (반복문 break)
+
+@bot.command(name="stop")  # stop 명령어 실행 시 isTimetableStop = False 로 하여 시간 공지 멈춤 (반복문 break)
 async def stop(ctx):
-    MyClass.isTimetableStop = False
-    await sends(ctx, "시간표 공지를 취소했습니다.\nTimetable : " + str(MyClass.isTimetableStop))
+    MainTime.isTimetableStop = False
+    await sends(ctx, "시간표 공지를 취소했습니다.\nTimetable : " + str(MainTime.isTimetableStop))
 
 
 @bot.command(name="reload")
@@ -139,7 +145,7 @@ async def JsonReload(ctx):
 
 
 @bot.command(name="confirm")
-async def react_test(ctx, days:str):
+async def react_test(ctx, days: str):
     # 유저가 요청했던 채널로 전송합니다.
     for i in range(1, 8):
         await embedSends(ctx, days, str(i))
@@ -147,10 +153,10 @@ async def react_test(ctx, days:str):
     return None
 
 
-@bot.command(name="timeNow")  # 현재 시간 출력
+@bot.command(name="now")  # 현재 시간 출력
 async def TimePrint(ctx):
     await ctx.channel.send(timeSet())
-    await ctx.channel.send(MyClass.st+" st 변수 출력")
+    await ctx.channel.send(MainTime.st + " st 변수 출력")
     return None
 
 
